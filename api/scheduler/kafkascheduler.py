cb215f62cebe191bc1eea99929403197a90948f9
Jay <jayaram.s@sentinelprotocol.io>
2019-09-23 11:50:37 +0800

    start scheduler in staging

--- api/scheduler/kafkascheduler.py
+++ api/scheduler/kafkascheduler.py
@@ -7,6 +7,5 @@ from api.settings import api_settings
 def start():
     indicator_listener = Listener_Indicator()
     scheduler = BackgroundScheduler()
-    if api_settings.PORTAL_API_MODE == "production":
-        scheduler.add_job(indicator_listener.check_for_new_cases, 'interval', minutes=5)
-        scheduler.start()
+    scheduler.add_job(indicator_listener.check_for_new_cases, 'interval', minutes=5)
+    scheduler.start()
