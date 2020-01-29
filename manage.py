ee943b9e24b5f5a18e959c608b8b14af0a268d7b
Adeel Siddiqui <34234631+rmiaas@users.noreply.github.com>
2020-01-29 17:06:23 +0800

    Send celery debug logs to Logstash

--- manage.py
+++ manage.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python
+import logging
 import os
 import sys
+
+from celery.signals import after_setup_logger, after_setup_task_logger
+import logstash
+
 from portal_api import AppInit
+
+
+def setup_logstash_celery(logger=None, loglevel=logging.DEBUG, **kwargs):
+    handler = logstash.TCPLogstashHandler(os.environ.get('API_LOGSTASH_SERVER', 'localhost'), 5959, tags=['celery'])
+    handler.setLevel(loglevel)
+    logger.addHandler(handler)
+    return logger
+
 
 if __name__ == "__main__":
     AppInit()
+    after_setup_task_logger.connect(setup_logstash_celery)
+    after_setup_logger.connect(setup_logstash_celery)
     env = os.environ.get("PORTAL_API_ENV")
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portal_api.settings.{env}".format(env=env))
     try:
