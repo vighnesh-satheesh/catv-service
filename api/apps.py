e2d1107d2798052b1d520d2eba1ce37d5442c55b
Jayaram Suresh <50855286+jayUppsala@users.noreply.github.com>
2020-04-28 17:44:14 +0800

    Update apps.py
    running scheduler in stage

--- api/apps.py
+++ api/apps.py
@@ -44,11 +44,11 @@ class ApiConfig(AppConfig):
         )
 
     def ready(self):
-        if os.environ.get("CONTAINER_TYPE", None) == "portal-api":
-            self.init_cache(self.get_model('User'))
-            self.send_slack_webhook()
-            from api.scheduler import kafkascheduler
-            kafkascheduler.start()
-            import api.signals
-            # import search_indexes.signals
+        self.init_cache(self.get_model('User'))
+        self.send_slack_webhook()
+        from api.scheduler import kafkascheduler
+        kafkascheduler.start()
+        import api.signals
+        # import search_indexes.signals
 
+            
