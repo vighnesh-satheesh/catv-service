4992b4118fd52285293ddae626a55481e22ac13e
Kaustav98 <31511879+Kaustav98@users.noreply.github.com>
2021-09-22 20:37:46 +0530

    Update exceptions.py

--- api/exceptions.py
+++ api/exceptions.py
@@ -8,64 +8,17 @@ from rest_framework import exceptions
 # 400
 #####
 
+
 class ValidationError(exceptions.APIException):
     status_code = status.HTTP_400_BAD_REQUEST
     default_detail = _('Invalid input.')
     default_code = 'invalid'
 
 
-class AuthenticationValidationError(ValidationError):
-    default_detail = _('valid email and password are required.')
-
-
-class EmailFormatError(ValidationError):
-    default_detail = _('email is not valid')
-
-
-class CaseFilterError(ValidationError):
-    default_detail = _('not supported case filter.')
-
-
-class CaseStatusChangeError(ValidationError):
-    default_detail = _('{from_status} cannot be changed to {to_status}')
-
-    def __init__(self, from_s, to_s):
-        detail = self.default_detail.format(from_status=from_s, to_status=to_s)
-        code = self.default_code
-
-        super().__init__(detail, code)
-
-
-class IndicatorDeleteError(ValidationError):
-    default_detail = _('case should be in progress status.')
-
-
-class FileNameTooLong(ValidationError):
-    default_detail = _('the length of filename should be less than 255.')
-
-
-class FileSizeTooSmall(ValidationError):
-    default_detail = _('the size of file should be larger than 50 bytes.')
-
-
-class FileNotAllowed(exceptions.APIException):
-    status_code = status.HTTP_403_FORBIDDEN
-    default_detail = _('Incorrect file type passed')
-
-
-class ICFAlreadyExist(ValidationError):
-    default_detail = _('icf api already exists.')
-
-
-class PasswordResetCodeNotValid(ValidationError):
-    default_detail = _('password reset code is not valid')
-
-class InvalidEthereumAddress(ValidationError):
-    default_detail = _('Invalid eth checksum address')
-
 #####
 # 401
 #####
+
 
 class AuthenticationCheckError(exceptions.AuthenticationFailed):
     default_detail = _('user not exist or password wrong.')
@@ -75,68 +28,34 @@ class AuthenticationCheckError(exceptions.AuthenticationFailed):
 # 404
 #####
 
+
 class UserNotFound(exceptions.NotFound):
     default_detail = _('user not found')
-
-
-class CaseNotFound(exceptions.NotFound):
-    default_detail = _('case not found')
-
-
-class IndicatorNotFound(exceptions.NotFound):
-    default_detail = _('indicator not found')
-
-
-class ICONotFound(exceptions.NotFound):
-    default_detail = _('ico not found')
-
 
 class FileNotFound(exceptions.NotFound):
     default_detail = _('file not found')
 
-class ICFNotFound(exceptions.NotFound):
-    default_detail = _('api not found')
-
-
-class OrganizationNotFound(exceptions.NotFound):
-    default_detail = _('organization not found')
-
 class CATVReportNotFound(exceptions.NotFound):
     default_detail = _('CATV Report not found')
+
 
 #####
 # 403
 #####
 
+
 class NotAllowedError(exceptions.PermissionDenied):
     default_detail = _('no permission.')
-
-
-class SupersentinelRequiredError(exceptions.PermissionDenied):
-    default_detail = _('supersentinel permission required.')
-
-
-class OwnerRequiredError(exceptions.PermissionDenied):
-    default_detail = _('case owner required.')
-
-
-class StatusChangeError(exceptions.PermissionDenied):
-    default_detail = _('no access to change the status')
 
 
 #####
 # 409
 #####
 
+
 class DataIntegrityError(exceptions.APIException):
     status_code = status.HTTP_409_CONFLICT
     default_detail = _('data conflict')
-
-
-class RequestTimeoutError(exceptions.APIException):
-    status_code = status.HTTP_408_REQUEST_TIMEOUT
-    default_detail = _('request timeout')
-
 
 class ServerError(exceptions.APIException):
     status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
