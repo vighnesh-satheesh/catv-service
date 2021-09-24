1609ba10a9562420158c2d9a28ac0245bb114881
Kaustav98 <31511879+Kaustav98@users.noreply.github.com>
2021-09-24 13:16:45 +0530

    Update base.py

--- portal_api/settings/base.py
+++ portal_api/settings/base.py
@@ -49,7 +49,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'django_extensions',
-    'social_django',
     'api'
 ]
 
@@ -90,32 +89,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'portal_api.wsgi.application'
 
-# Password validation
-# https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-        'OPTIONS': {
-            'user_attributes': ('nickname', 'email')
-        }
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
 
 AUTHENTICATION_BACKENDS = (
-    'social_core.backends.open_id.OpenIdAuth',
-    'social_core.backends.google.GoogleOpenId',
-    'social_core.backends.google.GoogleOAuth',
-    'social_core.backends.google.GoogleOAuth2',
     'django.contrib.auth.backends.ModelBackend',
 )
 
@@ -216,27 +191,6 @@ CORS_ALLOW_HEADERS = default_headers + (
 # file stoage
 
 DEFAULT_FILE_STORAGE = "api.storages.s3.S3Storage"
-
-# Python Social Auth (PSA) settings
-SOCIAL_AUTH_POSTGRES_JSONFIELD = True
-SOCIAL_AUTH_USERNAME_IS_FULL_EMAIL = True
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = env.str('API_SOCIAL_AUTH_GOOGLE_OAUTH2_KEY', '')
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = env.str('API_SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET', '')
-SOCIAL_AUTH_USER_MODEL = 'api.User'
-SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = ['email', 'profile']
-SOCIAL_AUTH_PIPELINE = (
-    'social_core.pipeline.social_auth.social_details',
-    'social_core.pipeline.social_auth.social_uid',
-    'social_core.pipeline.social_auth.auth_allowed',
-    'social_core.pipeline.social_auth.social_user',
-    'api.social_pipelines.get_username',
-    'social_core.pipeline.social_auth.associate_by_email',
-    'api.social_pipelines.create_user',
-    'social_core.pipeline.social_auth.associate_user',
-    'social_core.pipeline.social_auth.load_extra_data',
-    'social_core.pipeline.user.user_details',
-)
-
 
 # API Settings
 
