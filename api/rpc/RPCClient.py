31913259e3f16e307b818fbaa424dd389316788a
Kaustav98 <31511879+Kaustav98@users.noreply.github.com>
2021-09-27 12:55:50 +0530

    Update RPCClient.py

--- api/rpc/RPCClient.py
+++ api/rpc/RPCClient.py
@@ -9,15 +9,15 @@ class RPCClientUpdateUsageCatvCall:
         self.connection = pika.BlockingConnection(
             pika.ConnectionParameters(host='10.12.50.101'))
 
-    self.channel = self.connection.channel()
+        self.channel = self.connection.channel()
 
-    result = self.channel.queue_declare(queue='', exclusive=True)
-    self.callback_queue = result.method.queue
+        result = self.channel.queue_declare(queue='', exclusive=True)
+        self.callback_queue = result.method.queue
 
-    self.channel.basic_consume(
-        queue=self.callback_queue,
-        on_message_callback=self.on_response,
-        auto_ack=True)
+        self.channel.basic_consume(
+            queue=self.callback_queue,
+            on_message_callback=self.on_response,
+            auto_ack=True)
 
     def on_response(self, ch, method, props, body):
         if self.corr_id == props.correlation_id:
