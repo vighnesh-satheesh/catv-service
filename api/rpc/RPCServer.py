df1a9f9a427f6893f7d76a8cffcb2303fc6ef923
Ridhi M Jain <ridzz3419@gmail.com>
2021-11-29 11:08:58 +0530

    rpc changes for usage-stats

---
+++ api/rpc/RPCServer.py
@@ -0,0 +1,82 @@
+import ast
+import threading
+import json
+import time
+
+import pika
+from datetime import date, datetime
+from uuid import UUID, uuid4
+
+from dateutil.relativedelta import relativedelta
+from django.conf import settings
+from django.db import connection, connections, transaction
+from django.db.models import Q
+from django.db.models.functions import Lower
+from django.core.files.base import ContentFile
+from django.utils.translation import ugettext_lazy as _
+
+from api.constants import Constants
+from api.multitoken.tokens_auth import MultiToken
+
+from api.settings import api_settings
+
+class AMQPCATVConsuming(threading.Thread):
+
+    @staticmethod
+    def _get_connection():
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host=api_settings.RABBIT_MQ_URL))
+        return connection
+
+    def catv_usage(self, user, tz, date_range):
+        try:
+            query_list = Constants.QUERIES['CATV_USAGE_QUERY'].format(
+                tz, date_range, user)
+            with connection.cursor() as cursor:
+                cursor.execute(query_list)
+                result = cursor.fetchall()
+
+        except Exception as e:
+            print(f"Exception in updating catv usage - {e}")
+            return False
+        return result
+
+    def on_request_portalCatv_call(self, ch, method, props, body):
+        
+        # response = body.decode('utf-8')
+        result = ast.literal_eval(body.decode('utf-8'))
+        user = result['user_id']
+        tz = result['tz']
+        date_range = result['date_range']
+        response = self.catv_usage(user, tz, date_range)
+        if result:
+            print(response)
+            ch.basic_publish(exchange='',
+                             routing_key=props.reply_to,
+                             properties=pika.BasicProperties(correlation_id= \
+                                                                 props.correlation_id),
+                             body=str(response))
+            ch.basic_ack(delivery_tag=method.delivery_tag)
+        else:
+            ch.basic_publish(exchange='',
+                             routing_key=props.reply_to,
+                             properties=pika.BasicProperties(correlation_id= \
+                                                                 props.correlation_id),
+                             body=0)
+            ch.basic_ack(delivery_tag=method.delivery_tag)
+
+    def run(self):
+        # time.sleep(60)
+        connection = self._get_connection()
+        channel = connection.channel()
+
+        channel.queue_declare(queue='rpc_portal_catv_call')
+
+        channel.basic_qos(prefetch_count=20)
+
+        channel.basic_consume(queue='rpc_portal_catv_call',
+                on_message_callback=self.on_request_portalCatv_call)
+
+        print("[x] Awaiting Portal RPC requests")
+        channel.start_consuming()
+        
