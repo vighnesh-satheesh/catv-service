5cf6a27a0d64e982c0d070bc8c21f5983de6e6df
Vighnesh Satheesh <89012571+vighnesh-satheesh@users.noreply.github.com>
2022-02-03 15:47:47 +0530

    MSPORTAL-29 catv initial commit

--- api/router.py
+++ api/router.py
@@ -24,15 +24,15 @@ if settings.EXPOSE_INTERNAL_API:
 if settings.EXPOSE_GENERAL_API:
     urlpatterns += [
         url(r'^catv/?$', views.CATVView.as_view(), name='catv-view'),
-        url(r'^catvbtctracking/?$',
+        url(r'^catv_btc_tracking/?$',
             views.CATVBTCView.as_view(), name='catv-btc-view'),
-        url(r'^catvbtctxlist/?$', views.CATVBTCTxlistView.as_view(),
+        url(r'^catv_btc_txlist/?$', views.CATVBTCTxlistView.as_view(),
             name='catv-btc-txlist-view'),
-        url(r'^catvhistory/?$', views.CATVHistoryView.as_view(), name='catv-history'),
-        url(r'^catvrequests/?$', views.CATVRequestsView.as_view(), name='catv-requests'),
-        url(r'^catvreport/(?P<pk>[0-9a-z\-]+)/?$', views.CATVReportView.as_view(), name='catv-report'),
-        url(r'^catvmultireport/?$', views.CATVMultiReportView.as_view(), name='catv-multi-report'),
-        url(r'^catvrequests/(?P<pk>[0-9a-z\-]+)/?$', views.CATVRequestDetailView.as_view(),
+        url(r'^catv_history/?$', views.CATVHistoryView.as_view(), name='catv-history'),
+        url(r'^catv_requests/?$', views.CATVRequestsView.as_view(), name='catv-requests'),
+        url(r'^catv_report/(?P<pk>[0-9a-z\-]+)/?$', views.CATVReportView.as_view(), name='catv-report'),
+        url(r'^catv_multi_report/?$', views.CATVMultiReportView.as_view(), name='catv-multi-report'),
+        url(r'^catv_requests/(?P<pk>[0-9a-z\-]+)/?$', views.CATVRequestDetailView.as_view(),
             name='catv-request-detail'),
         url(r'^request_search/?$', views.RequestSearchView.as_view(), name='request-search'),
     ]
