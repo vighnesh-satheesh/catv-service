e11439affd26eeb46269b4157d8ff766d2e09baa
Kaustav98 <31511879+Kaustav98@users.noreply.github.com>
2022-03-31 18:16:47 +0530

    Update router.py

--- api/router.py
+++ api/router.py
@@ -34,5 +34,5 @@ if settings.EXPOSE_GENERAL_API:
         url(r'^catvmultireport/?$', views.CATVMultiReportView.as_view(), name='catv-multi-report'),
         url(r'^catvrequests/(?P<pk>[0-9a-z\-]+)/?$', views.CATVRequestDetailView.as_view(),
             name='catv-request-detail'),
-        url(r'^requestsearch/?$', views.RequestSearchView.as_view(), name='request-search'),
+        url(r'^request_search/?$', views.RequestSearchView.as_view(), name='request-search'),
     ]
