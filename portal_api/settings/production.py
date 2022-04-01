d88edf34b9c3cfc971bd44f9bfc1084c8c90e7da
Kaustav98 <31511879+Kaustav98@users.noreply.github.com>
2022-04-01 21:02:12 +0530

    Update production.py

--- portal_api/settings/production.py
+++ portal_api/settings/production.py
@@ -7,7 +7,7 @@ DEBUG = False
 
 ALLOWED_HOSTS += env.list('ALLOWED_HOSTS', default=['*', ])
 ALLOWED_HOSTS += [
-    "localhost", "test.sentinelportal.com", "catv-service.api.sentinelprotocol.io"
+    "localhost", "test.sentinelportal.com", "catv-service.api.sentinelprotocol.io", "portal.sentinelprotocol.io"
 ]
 
 # Sentry
