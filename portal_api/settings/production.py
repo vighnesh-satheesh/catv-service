0223996f750341b79918568eee5334479dc0f99a
Vighnesh Satheesh <89012571+vighnesh-satheesh@users.noreply.github.com>
2023-05-22 14:05:23 +0530

    add apm config in production.py

--- portal_api/settings/production.py
+++ portal_api/settings/production.py
@@ -15,8 +15,27 @@ ALLOWED_HOSTS += [
 version = env.str('PORTAL_API_VERSION', None)
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-] + INSTALLED_APPS
+                     'elasticapm.contrib.django',
+                     'django.contrib.admin',
+                 ] + INSTALLED_APPS
+
+# For ElasticSearch logging
+"""
+Most probably we don't need to change the middleware on the next line,
+because Elastic APM inserts the APM middleware to the top of the middleware list by default
+https://www.elastic.co/guide/en/apm/agent/python/current/configuration.html#config-django-autoinsert-middleware
+Just to be double sure if the build fails
+"""
+MIDDLEWARE.insert(0, 'elasticapm.contrib.django.middleware.TracingMiddleware')
+ELASTIC_APM = {
+    # Set required service name. Allowed characters:
+    # a-z, A-Z, 0-9, -, _, and space
+    'SERVICE_NAME': env.str('API_APM_SERVICE_NAME', 'portal-catv-service'),
+    # Set custom APM Server URL (default: http://localhost:8200)
+    'SERVER_URL': env.str('API_ELASTIC_SERVER_URL', ''),
+    'SECRET_TOKEN': env.str('API_APM_SECRET', ''),
+    'ENVIRONMENT': env.str('CATVMS_API_ENV', '')
+}
 
 LOGGING = {
     'version': 1,
@@ -45,7 +64,7 @@ LOGGING = {
             'version': 1,
             'message_type': 'logstash',
             'fqdn': True,
-            'tags': ['django.request', 'django-portalapi'],
+            'tags': ['django.request', 'django-portal-catv'],
         },
     },
     'loggers': {
@@ -61,4 +80,4 @@ LOGGING = {
     }
 }
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
