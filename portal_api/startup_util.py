4e13bcb0e10f8e54e6c5d3dfa751c9e8ac3eb425
Siva-Surendranath <siva.s@sentinelprotocol.io>
2023-12-04 19:50:45 +0530

    adding more allowed hosts

--- portal_api/startup_util.py
+++ portal_api/startup_util.py
@@ -125,6 +125,8 @@ def set_allowed_hosts():
             'search.stgsentinelportal.com',
             'search.prdsentinelportal.com',
             '34.142.249.113',
+            '172.16.128.0/20',
+            '172.16.4.0/23',
             '10.80.0.0/16'
             
         ])
