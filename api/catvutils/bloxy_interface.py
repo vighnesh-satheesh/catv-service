17620c6e154a2dacf47c4441794761a784377f19
vighnesh-satheesh <vighnesh.satheesh@sentinelprotocol.io>
2024-02-23 15:08:45 +0530

    debugging bloxy_interface

--- api/catvutils/bloxy_interface.py
+++ api/catvutils/bloxy_interface.py
@@ -209,7 +209,8 @@ class GraphQLInterfaceUnified:
             r = requests.post(self._graphql_endpoint, json={
                 'query': request_body}, headers=self._headers, timeout=(self.connect_timeout, self.read_timeout))
             response = r.json()
-            print(request_body)
+            print("graphql query: ", request_body)
+            print("response: ", response)
             for item in response["data"][Constants.NETWORK_CHAIN_MAPPING_FOR_RESPONSE[self.chain]]["coinpath"]:
                 # These dict items are common to all response bodies
                 # After this, the code enters the nested if-else block and the other parameters are assigned
