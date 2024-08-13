7f5ba3b0bcb1aa09f28da60d608cc1cea04e72c4
vighnesh-satheesh <vighnesh.satheesh@sentinelprotocol.io>
2024-08-13 18:56:27 +0530

    update validate_dateformat_and_randomize_seconds()

--- api/catvutils/bloxy_interface.py
+++ api/catvutils/bloxy_interface.py
@@ -86,7 +86,7 @@ class GraphQLInterfaceUnified:
         self.source = source
         self.address = address
         self.depth = depth_limit
-        from_time = utils.validate_dateformat_and_randomize_seconds(from_time,'%Y-%m-%d',"%Y-%m-%dT%H:%M:%S")
+        from_time = utils.validate_dateformat_and_randomize_seconds(from_time,"%Y-%m-%dT%H:%M:%S")
         self.from_time = str(from_time).replace(" ", "T")
         self.till_time = str(till_time).replace(" ", "T")
         self.limit = int(limit)
