9c2a87cb9b1b2fc3ff4527d34fd27cb7e575ad11
vighnesh-satheesh <vighnesh.satheesh@sentinelprotocol.io>
2025-01-28 15:28:11 +0530

    removing elastic and updating docker

--- portal_api/settings/production.py
+++ portal_api/settings/production.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS += [
 version = env.str('PORTAL_API_VERSION', None)
 
 INSTALLED_APPS = [
-                     'elasticapm.contrib.django',
+                     # 'elasticapm.contrib.django',
                      'django.contrib.admin',
                  ] + INSTALLED_APPS
 
@@ -40,16 +40,16 @@ because Elastic APM inserts the APM middleware to the top of the middleware list
 https://www.elastic.co/guide/en/apm/agent/python/current/configuration.html#config-django-autoinsert-middleware
 Just to be double sure if the build fails
 """
-MIDDLEWARE.insert(0, 'elasticapm.contrib.django.middleware.TracingMiddleware')
-ELASTIC_APM = {
-    # Set required service name. Allowed characters:
-    # a-z, A-Z, 0-9, -, _, and space
-    'SERVICE_NAME': env.str('API_APM_SERVICE_NAME', 'portal-catv-service'),
-    # Set custom APM Server URL (default: http://localhost:8200)
-    'SERVER_URL': env.str('API_ELASTIC_SERVER_URL', ''),
-    'SECRET_TOKEN': env.str('API_APM_SECRET', ''),
-    'ENVIRONMENT': env.str('CATVMS_API_ENV', '')
-}
+# MIDDLEWARE.insert(0, 'elasticapm.contrib.django.middleware.TracingMiddleware')
+# ELASTIC_APM = {
+#     # Set required service name. Allowed characters:
+#     # a-z, A-Z, 0-9, -, _, and space
+#     'SERVICE_NAME': env.str('API_APM_SERVICE_NAME', 'portal-catv-service'),
+#     # Set custom APM Server URL (default: http://localhost:8200)
+#     'SERVER_URL': env.str('API_ELASTIC_SERVER_URL', ''),
+#     'SECRET_TOKEN': env.str('API_APM_SECRET', ''),
+#     'ENVIRONMENT': env.str('CATVMS_API_ENV', '')
+# }
 
 LOGGING = {
     'version': 1,
